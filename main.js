fd94c0cb334943b93dbb1a9b71635dc140a10398
Quinten Preiß <90014830+J-C-Q@users.noreply.github.com>
2022-01-21 23:09:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -14,16 +14,40 @@ function Range(a,b,N) {
   return x;
 }
 
-function exponentialtypebeat(x) {
+function exponentialtypebeat(r,x) {
   var a = [];
   for (var i = 0; i<x.length;i++) {
-    a.push(x[i]*Math.exp((1-x[i]))); 
+    a.push(x[i]*Math.exp(r*(1-x[i]))); 
   }
   return a;
 }
 
-var x = Range(0,10,100);
-var f = exponentialtypebeat(x);
+function removeData(chart) {
+  chart.data.labels.pop();
+  chart.data.datasets.forEach((dataset) => {
+      dataset.data.pop();
+  });
+  chart.update();
+}
+
+function addData(chart, label, data) {
+  chart.data.labels.push(label);
+  chart.data.datasets.forEach((dataset) => {
+      dataset.data.push(data);
+  });
+  chart.update();
+}
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("value");
+output.innerHTML = slider.value/40;
+
+slider.oninput = function() {
+  output.innerHTML = this.value/40;
+}
+
+var x = Range(0,10,500);
+var f = exponentialtypebeat(0,x);
 
 let delayed;
 var ctx = document.getElementById('myChart').getContext('2d');
@@ -32,18 +56,24 @@ var chart = new Chart(ctx, {
   data: {
     labels: x,
     datasets: [{
-      label: 'x*e^(1-x)',
+      label: 'x*e^(r-rx)',
       backgroundColor: 'rgb(0,0,0)',
       borderColor: 'rgb(0,0,0)',
       data: f,
+    },{
+      label: 'x',
+      backgroundColor: 'rgb(0,0,0)',
+      borderColor: 'rgb(0,0,0)',
+      data: x,
     }]
   },
   options: {
+    showPoint: false,
     scales: {
         x: {
           type: 'linear',
           min: 0,
-          max: 4
+          max: 2
         },
         y: {
           type: 'linear',
@@ -53,21 +83,27 @@ var chart = new Chart(ctx, {
             }
           },
           min: 0,
-          max: 1.2
+          max: 2
         }
     },
     resposive: true,
-    animation: {
-      onComplete: () => {
-        delayed = true;
-      },
-      delay: (context) => {
-        let delay = 0;
-        if (context.type === 'data' && context.mode === 'default' && !delayed) {
-          delay = context.dataIndex * 100 + context.datasetIndex * 1;
-        }
-        return delay;
-      },
-  }
+    pointRadius: 0
   }
 });
+
+
+slider.addEventListener("mousemove", function() {
+  var l = slider.value;
+  var color = 'linear-gradient(90deg,rgb(0,0,117)'+l+'%, rgb(214,214,214)'+l+'%)';
+  slider.style.background=color;
+  let newthig = l/40
+  f = exponentialtypebeat(newthig,x);
+  chart.data.datasets[0].data = f;
+  chart.update('none');
+})
+
+
+
+
+
+
